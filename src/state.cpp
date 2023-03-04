76a82134d8e6718bd080bf6343ef09889f58f5c3
xyderos <kostas@xyderos.com>
2023-03-04 11:48:31 +0100

    deleted unused files

--- src/state.cpp
+++
@@ -1,9 +0,0 @@
-#include "state.h"
-
-state::~state() = default;
-
-void
-state::set_context(context *c)
-{
-	this->ctx = c;
-}
--- src/state_machine.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-using state_machine_t = enum state_machine { A_STATE, ANOTHER_STATE, NO_STATE };
